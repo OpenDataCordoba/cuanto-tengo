6e5903436875c38ca1f11000e6fccbc6e46d2cad
Gaston Avila <avila.gas@gmail.com>
2017-09-16 13:55:55 -0300

    Apuntar a heroku

--- js/cuantoTengoApp.js
+++ js/cuantoTengoApp.js
@@ -1,4 +1,6 @@
 var app = angular.module("cuantoTengo", ['ui.bootstrap', "angular-uuid"]);
 var url_destino = "https://gobiernoabierto.cordoba.gob.ar/api";
 // var url_base = "https://200.123.180.122:5743";
-var url_base = "http://localhost:8000";
+// Desarrollo
+// var url_base = "http://localhost:8000";
+var url_base = "https://cunto-tengo.herokuapp.com";
