6436dab4cee59f45e405380779013e568be2ddf6
Gaston Avila <avila.gas@gmail.com>
2017-09-16 15:55:55 -0300

    A produccion

--- js/cuantoTengoApp.js
+++ js/cuantoTengoApp.js
@@ -2,5 +2,5 @@ var app = angular.module("cuantoTengo", ['ui.bootstrap', "angular-uuid"]);
 var url_destino = "https://gobiernoabierto.cordoba.gob.ar/api";
 // var url_base = "https://200.123.180.122:5743";
 // Desarrollo
-var url_base = "http://dev.localhost:5000";
-// var url_base = "https://cuanto-tengo.herokuapp.com";
+// var url_base = "http://dev.localhost:5000";
+var url_base = "https://cuanto-tengo.herokuapp.com";
